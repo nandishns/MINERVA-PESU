41e414cf0872f58e2fa4f2616dd5f9e09eb8ed9a
Nandish N S <92267208+nandishns@users.noreply.github.com>
2023-01-17 23:22:28 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import PESLogo from "../assets/pes-logo-white.png"
 import ClubLogo from "../assets/club-logo.png"
 export default function Footer() {
   return (
+    <>
     <footer
   className=" page-footer font-small cyan darken"
   style={{ backgroundColor: "#3c3635", borderTop: "2px solid #eceeef" }}
@@ -62,6 +63,12 @@ export default function Footer() {
     Made with ❣️ by Web Dev Team
   </div>
 </footer>
-
+ {/* <!-- Scroll Top --> */}
+ <a href="#" aria-label="scroll-up" class="scrollup" id="scroll-up">
+ <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+<path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+</svg>
+ </a>
+ </>
   )
 }
