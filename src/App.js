350dc9c975c397684d4168d57ffaa9fe753b8946
Nandish N S <92267208+nandishns@users.noreply.github.com>
2023-01-17 23:19:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar';
 import {Route,Routes}  from 'react-router-dom';
 import Footer from './Components/Footer';
 import HomePage from './Components/HomePage';
+import Article from './Components/Article';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Navbar />
     <Routes>
     <Route exact path="/" element={<HomePage/>} ></Route>
+    <Route exact path="/article" element={<Article/>} ></Route>
     </Routes>
     <Footer/>
     </>
